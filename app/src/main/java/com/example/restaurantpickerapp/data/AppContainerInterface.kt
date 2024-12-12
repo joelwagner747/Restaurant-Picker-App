0675de7717f608eb26f757960261b010239767f8
Joel Wagner <joelwagner747@gmail.com>
2024-12-12 13:59:04 -0700

    Cleaned for Pages

--- app/src/main/java/com/example/restaurantpickerapp/data/AppContainerInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.restaurantpickerapp.data
-
-interface AppContainerInterface {
-    val placesRepository: PlacesRepository
-
-    val selectionOptionsRepository: SelectionOptionsRepositoryInterface
-
-    val restaurantRepository: RestaurantRepository
-}
--- app/src/main/java/com/example/restaurantpickerapp/ui/navigation/NavigationDestination.kt
+++
@@ -1,9 +0,0 @@
-package com.example.restaurantpickerapp.ui.navigation
-
-import androidx.annotation.StringRes
-
-interface NavigationDestination {
-    val route: String
-
-    val titleResource: Int
-}
